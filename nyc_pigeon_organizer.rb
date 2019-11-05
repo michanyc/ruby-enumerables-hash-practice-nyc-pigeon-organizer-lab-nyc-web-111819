8e56741f4162579bb9850e244892dc8f0f4a097b
Michael Gerland <mr.gerland@gmail.com>
2019-11-05 15:39:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
+  new_hash = {}
+    data.each do |key, value|
+      value.each do |value2, names|
+        names.each do |name|
+
+          if new_hash[name] == nil
+            new_hash[name] = {}
+          end
+          if new_hash[name][key] == nil
+            new_hash[name][key] = []
+          end
+          new_hash[name][key] << value2.to_s
+        end
+      end
+    end
+
+  new_hash
+
   # write your code here!
 end
